d967a21691ee83271668b7f734eaea0b038d28b4
Develpjuan <romeoquintero0@gmail.com>
2026-05-19 17:07:49 -0500

    Imc

--- retos.py
+++ retos.py
@@ -1,1 +1,16 @@
 #Aqui van las funciones
+def calcular_BMI (peso_lb, altura_inch):
+        peso_lb = peso_lb * 0.45
+        altura_metros = altura_inch * 0.025
+        BMI = peso_lb / (altura_metros ** 2)
+        return round (BMI, 2)
+def calcular_cambio (cambio: int):
+    moneda500 = cambio // 500
+    cambio %=500
+    moneda200 = cambio // 200
+    cambio %=200
+    moneda100 = cambio // 100
+    cambio %=100
+    moneda50 = cambio // 50
+    cambio %=50
+    return str(moneda500) + "," + str(moneda200) + "," + str(moneda100) + "," + str(moneda50)
